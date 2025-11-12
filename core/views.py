c4c14fe49525466caddee9d1073d5f309c25aa93
djvjr <djvjr@iamdevice.com>
2025-11-13 07:26:08 +0900

    comment out2

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .models import Post
-#from ratelimit.decorators import ratelimit
+from django_ratelimit.decorators import ratelimit
 
 def signup_view(request):
     if request.method == 'POST':
@@ -20,7 +20,7 @@ def index(request):
     return render(request, 'core/index.html', {'posts': posts})
 
 @login_required
-#@ratelimit(key='user', rate='10/m', method='POST', block=True)
+@ratelimit(key='user', rate='10/m', method='POST', block=True)
 def post_create(request):
     if request.method == 'POST':
         if getattr(request,'limited',False):
@@ -36,11 +36,10 @@ def post_create(request):
 # core/views.py
 from django.contrib.auth.views import LoginView
 from django.utils.decorators import method_decorator
-#from ratelimit.decorators import ratelimit
 from django.contrib import messages
 
 # POSTメソッドの試行をIP単位で 5回/分 に制限。超過時は 429 を返す。
-#@method_decorator(ratelimit(key='ip', rate='5/m', method='POST', block=True), name='dispatch')
+@method_decorator(ratelimit(key='ip', rate='5/m', method='POST', block=True), name='dispatch')
 class RateLimitedLoginView(LoginView):
     template_name = 'core/login.html'
 
