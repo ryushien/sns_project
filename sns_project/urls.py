32dc7c49bceb4c30ceebb99bdc927bb2b4d1a593
djvjr <djvjr@iamdevice.com>
2025-11-13 06:43:19 +0900

    remove duplicate urls.py

--- sns_project/urls.py
+++ sns_project/urls.py
@@ -14,20 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# sns_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
-from ratelimit.decorators import ratelimit
-
-# ここで LoginView をデコレータで包む（core.views を import しない）
-login_view = ratelimit(key='ip', rate='5/m', method='POST', block=True)(
-    LoginView.as_view(template_name='core/login.html')
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('login/', login_view, name='login'),
+    path('login/', LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
+
