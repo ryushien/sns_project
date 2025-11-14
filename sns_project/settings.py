9d44ed91d6d4c7bbd918083fdc1d57012090795e
djvjr <djvjr@iamdevice.com>
2025-11-14 14:10:45 +0900

    act logout

--- sns_project/settings.py
+++ sns_project/settings.py
@@ -44,7 +44,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/login/"
+LOGOUT_REDIRECT_URL = "/"
 
 INSTALLED_APPS = [
     'django.contrib.admin',
