1bcd649b0b691633d9973a808d5e447e02490be8
djvjr <djvjr@iamdevice.com>
2026-05-18 23:57:57 +0900

    AWS S3ストレージの設定を追加

--- sns_project/settings.py
+++ sns_project/settings.py
@@ -63,7 +63,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',  # あなたのアプリ
+    'storages',
 ]
+
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
+AWS_S3_REGION_NAME = "ap-northeast-1"
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 TEMPLATES = [
     {
